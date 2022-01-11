9d86c7d5db8dd9310bdc0c5df18de6f4358de04f
stepper3117 <91539425+stepper3117@users.noreply.github.com>
2022-01-11 17:30:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function submit()
+
+var display_student_array = [];
+
+for(var j = 1; <= 4; j++)
+
+var name_of_the_student = document.getElementById("name_of_the_students"=j).value;
+console.log(name_of_the_student);
+name_of_the_student_array.push(name_of_the_student);
+
+console.log(name_of_the_student_array);
+
+var lenght_of_name_of_students_array
